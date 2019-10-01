23d8c1f55d8d5c37cd5ab545a6b4a8b8d4d86aa4
Jason Moon <jasonmoonjm@gmail.com>
2019-10-01 15:57:17 -0700

    pet constructor

---
+++ js/app.js
@@ -0,0 +1,21 @@
+const name = prompt('What would you like to name your pet?')
+
+class Pet {
+    constructor(name){
+        this.name = name; 
+    }
+    boredom = 0;
+    sleepiness = 0;
+    hunger = 0;
+    age = 0; // increase every x minutes  & at certain ages morph your pet 
+}
+
+const game = {
+    start(){
+        // block of code
+    }
+}
+
+const userPet = new Pet(name);
+
+
