0629fce804d584951a08e455258702a6eba4310b
Jason Moon <jasonmoonjm@gmail.com>
2019-10-02 11:26:35 -0700

    pet methods and buttons finished

--- js/app.js
+++ js/app.js
@@ -1,17 +1,39 @@
 // const name = prompt('What would you like to name your pet?')
+const buttonsHandler = () =>{
+    $('.create').on('click', () => {
+        $('#pet').css('display', 'block');
+    });
+    game.buttons(userPet);
+}
 
 class Pet {
     constructor(name){
         this.name = name; 
+        this.boredom = 0;
+        this.sleepiness = 0;
+        this.hunger = 0;
+        this.age = 0; // increase every x minutes  & at certain ages morph your pet 
     }
-    boredom = 0;
-    sleepiness = 0;
-    hunger = 0;
-    age = 0; // increase every x minutes  & at certain ages morph your pet 
+    bore(){
+        this.boredom++;
+        $('#pet .boredom').text(`Boredom: ${this.boredom}`);
+    };
+    sleepy(){
+        this.sleepiness++;
+        $('#pet .sleepiness').text(`Sleepiness: ${this.sleepiness}`);
+    };
+    starve(){
+        this.hunger++;
+        $('#pet .hunger').text(`Hunger: ${this.hunger}`);
+    };
+    grow(){
+        this.age++;
+        $('#pet .age').text(`Age: ${this.age}`);
+    };
 }
 
 const game = {
-    time: 30,
+    time: 30, //set to 60 later 30 for debugging
     setTime(pet) {
         const timer = setInterval(() => {
             this.time--;
@@ -22,14 +44,17 @@ const game = {
                 console.log('Your pet died');
             };
             
+            // if (pet.age === 2 || pet.age === 4)
+            //  run can morph 
+            
             if (this.time === 25) {
-                pet.hunger++; 
+                pet.starve(); 
             } else if (this.time === 20) {
-                pet.boredom++;
+                pet.bore();
             } else if (this.time === 10) {
-                pet.sleepiness++;
+                pet.sleepy();
             } else if(this.time === 5) {
-                pet.age++;
+                pet.grow();
             } else if (this.time === 0) {
                 this.time = 30;
             };
@@ -40,9 +65,32 @@ const game = {
         if (pet.boredom === 2 || pet.sleepiness === 2 || pet.hunger === 2) {
             return true;
         };
+    },
+    canMorph(pet){
+        if (pet.age === 2) {
+           // manipulate to one pic 
+        } else {
+           // manipulate to other pic
+        }
+    },
+    buttons(pet){  
+        $('.btns').on('click', (e) => {
+            if ($(e.target).text() === 'Feed' && pet.hunger > 0) {
+                pet.hunger--;
+                $('#pet .hunger').text(`Hunger: ${pet.hunger}`);
+            } else if ($(e.target).text() === 'Rest' && pet.sleepiness > 0) {
+                pet.sleepiness--;
+                $('#pet .sleepiness').text(`Sleepiness: ${pet.sleepiness}`);
+            } else if ($(e.target).text() === 'Play' && pet.boredom > 0) {
+                pet.boredom--;
+                $('#pet .boredom').text(`Boredom: ${pet.boredom}`);
+            }
+        })
     }
 }
 
+
 const userPet = new Pet('jason');
+buttonsHandler();
 
 // map the buttons 
