08c127d6687f13136f35003a3cd590df94ef4816
Jason Moon <jasonmoonjm@gmail.com>
2019-10-01 22:22:51 -0700

    pet timer function added

--- js/app.js
+++ js/app.js
@@ -11,12 +11,38 @@ class Pet {
 }
 
 const game = {
-    start(){
-        // block of code
-        
+    time: 30,
+    setTime(pet) {
+        const timer = setInterval(() => {
+            this.time--;
+            console.log(this.time);
+            console.log(pet);
+            if(this.isDead(pet)){
+                clearInterval(timer);
+                console.log('Your pet died');
+            };
+            
+            if (this.time === 25) {
+                pet.hunger++; 
+            } else if (this.time === 20) {
+                pet.boredom++;
+            } else if (this.time === 10) {
+                pet.sleepiness++;
+            } else if(this.time === 5) {
+                pet.age++;
+            } else if (this.time === 0) {
+                this.time = 30;
+            };
+    
+        }, 1000)
+    },
+    isDead(pet){
+        if (pet.boredom === 2 || pet.sleepiness === 2 || pet.hunger === 2) {
+            return true;
+        };
     }
 }
 
-const userPet = new Pet(name);
+const userPet = new Pet('jason');
 
 
